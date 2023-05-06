9fcfc655905e17d97ba449ed317cf6320c136d85
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-05-07 02:27:45 +0700

    123

---
+++ .circleci/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST https://circleci.com/api/v1.1/project/github/3Kmfi6HP/argo-airport-paas/tree/circleci-project-setup/build -H "Circle-Token: 6ebbf6f52c0f44cd27887b2c1f217d5b44ed0417"
